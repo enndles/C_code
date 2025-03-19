9b6cb9ff07c694ef527a3778147b3c69d1990604
enndles <xiaoyaoyou2358@163.com>
2025-03-19 11:01:21 +0800

    测试提交

---
+++ Text_4/Text_4/text.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num % 2)
+//	{
+//		printf("是奇数");
+//	}
+//	else
+//	{
+//		printf("是偶数");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		if (i % 2 == 1)
+//		{
+//			printf("%d\n",i);
+//		}
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d",&day);
+//	switch (day)
+//	{
+//	case 1:
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = getchar();
+//	putchar(a);
+//	return 0;
+//
+// 
+// }
+//int main()
+//{
+//	int a = 0;
+//	while ((a = getchar()) != EOF)
+//	{
+//		putchar(a);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	//int arr[] = { 73,32,99,97,110,32,105,116,33 };
+//	//for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
+//	//{
+//	//	printf("%c",arr[i]);
+//	//}
+//
+//	//return 0;
+//	//int year = 0;
+//	//int month = 0;
+//	//int day = 0;
+//	//printf("请输入您的生日：");
+//	//scanf("%4d%2d%2d",&year,&month,&day);
+//	//printf("year=%d\n",year);
+//	//printf("month=%02d\n", month);
+//	//printf("day=%02d\n", day);
+//	int number = 0;
+//	double C = 0;
+//	double Math = 0;
+//	double English = 0;
+//	scanf("%d;%lf,%lf,%lf",&number,&C,&Math,&English);
+//	printf("The each subject score of No. %d is %.2lf, %.2lf, %.2lf.\n",number,C,Math,English);
+//}
+//int main()
+//{
+//	int m, n, a, b;
+//	scanf("%d %d %d %d",&m, &n, &a, &b);
+//	int max = m > (n > (a > b ? a : b) ? n : (a > b ? a : b)) ? m : (n > (a > b ? a : b) ? n : (a > b ? a : b));
+//	printf("%d\n",max);
+//	return 0;
+//}
+
+//int main()
+//{
+//	double pi = 3.1415926;
+//	double r = 0;
+//	scanf("%lf", &r);
+//	double area = (4 / 3.0) * pi * r * r * r;
+//	printf("%.3lf\n",area);
+//	return 0;
+//}
